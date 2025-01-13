1e9d070ff5f096b25654e8cba523c8872fc02b85
Luis Rafael Sanchez <92004025+Sirenth@users.noreply.github.com>
2025-01-13 13:29:13 -0600

    Avance - Falta aún diseñar cosasa
    NO ESTA TERMINADO AÚN

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import Splide from "@splidejs/splide";
+import "./bootstrap";
+
+var splide = new Splide(".splide", {
+    type: "loop",
+    padding: "5rem",
+    gap: "1rem",
+});
+
+splide.mount();
