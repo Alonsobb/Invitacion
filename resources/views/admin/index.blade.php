0301a43ba17f3779f4e516429421c49cd7aeec37
Alonso Bravo Bernal <58313418+Alonsobb@users.noreply.github.com>
2025-01-24 11:17:09 -0600

    Total de invitados

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,33 +1,45 @@
 @extends('layout.index')
 
 @section('app')
-    <div class="flex flex-wrap gap-4 mx-16">
-        <div class="card w-[450px]">
-            <div class="card-content">
-                <p class="title  bg-slate-400">
-                   Chanell lleva
-                   @if (isset($Invitados_novia))    
-                   {{$Invitados_novia}} 
-                   @endif
-                   Invitados adultos @if($niños[0]!=0) y 
-                       {{$niños[0]}} niños
-                   @endif
-                </p>
+    <div class="flex justify-center flex-col mt-[150px] ">
+        <div class="flex flex-wrap gap-6 justify-center">
+            <div class="card  bg-fuchsia-500  w-1/3 border h-28 ">
+                <div class="card-content w-full ">
+                    <p class="title text-center">
+                        Chanell lleva
+                        @if (isset($Invitados_novia))
+                            {{ $Invitados_novia }}
+                        @endif
+                        Invitados adultos @if ($niños[0] != 0)
+                            y
+                            {{ $niños[0] }} niños
+                        @endif
+                    </p>
+                </div>
+            </div>
+            <div class="card bg-indigo-300 w-1/3 h-28">
+                <div class="card-content">
+                    <p class="title text-center">
+                        Alonso lleva
+                        @if (isset($Invitados_novio))
+                            {{ $Invitados_novio }}
+                        @endif
+                        Invitados adultos @if ($niños[1] != 0)
+                            y
+                            {{ $niños[1] }} niños
+                        @endif
+                    </p>
+                </div>
             </div>
         </div>
-        <div class="card w-[450px]">
-            <div class="card-content">
-                <p class="title bg-slate-400">
-                    Alonso lleva
-                   @if (isset($Invitados_novio))    
-                   {{$Invitados_novio}} 
-                   @endif
-                   Invitados adultos @if($niños[1]!=0) y 
-                   {{$niños[1]}} niños
-               @endif
-                </p>
+        <div class="flex flex-wrap justify-center">
+            <div class="card bg-green-400 w-1/3 h-28">
+                <div class="card-content">
+                    <p class="title text-center">
+                        El total de invitados son: {{$Invitados_novia+$Invitados_novio+$niños[0]+$niños[1]}}
+                    </p>
+                </div>
             </div>
-
         </div>
     </div>
 @endsection
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -9,8 +9,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <!-- Styles / Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         @yield('css');
